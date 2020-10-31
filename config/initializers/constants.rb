fad8abc7e53371c5c3900a2645ef9ce0ee19c2b5
Ranjit <ranzit005@gmail.com>
2020-10-31 17:51:05 +0530

    api url app

--- config/initializers/constants.rb
+++ config/initializers/constants.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 if Rails.env.production?
-  AUTH_APP_URL = 'http://localhost:3000/api/v1'
+  AUTH_APP_URL = 'http://customer-omniapp.herokuapp.com/api/v1'
 else
   AUTH_APP_URL = 'http://localhost:3000/api/v1'
 end
